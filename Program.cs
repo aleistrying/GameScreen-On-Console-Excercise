d7e9f9ac355bb4619e3243ec1ff82cba1693d06f
Alejandro P <alejandroparparcen@yahoo.com>
2019-04-13 22:17:04 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Excercise15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=============");
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1 − New Game");
+            Console.WriteLine("2 − Load Game");
+            Console.WriteLine("3 − Options");
+            Console.WriteLine("4 − Quit");
+            Console.WriteLine("=============");
+            Console.WriteLine("");
+            Console.Write("Select the option from the menu");
+            Console.WriteLine();
+            int input = int.Parse(Console.ReadLine());
+            Console.WriteLine("Loading  game . . .");
+            if(input == 1)
+            {
+                Console.WriteLine("You selected a new game file");
+            }
+            else if (input == 2)
+            {
+                Console.WriteLine("You selected to load your previous game");
+            }
+            else if (input == 3)
+            {
+                Console.WriteLine("You selected the Options menu");
+            }
+            else if (input == 4)
+            {
+                Console.WriteLine("Quitting game . . .");
+            }
+            else
+            {
+                Console.WriteLine("You wrote an invalid value");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Press any key to quit . . .");
+            Console.ReadKey();
+        }
+    }
+}
